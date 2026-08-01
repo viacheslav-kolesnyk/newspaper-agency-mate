aead88cf655078f04ee496a3b1b3afaba0139ae6
Viakol <viakol.lokaiv@gmail.com>
2026-08-01 16:52:22 +0300

    Register Topic, Editor, Newspaper models in admin

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Topic, Editor, Newspaper
 
-# Register your models here.
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    search_fields = ["name"]
+
+@admin.register(Editor)
+class EditorAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "first_name", "last_name", "years_of_experience"]
+    list_filter = ["groups"]
+    search_fields = ["username", "first_name", "last_name"]
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "published_date", "editor"]
+    list_filter = ["published_date", "topics"]
+    search_fields = ["title", "content"]
+    date_hierarchy = "published_date"
